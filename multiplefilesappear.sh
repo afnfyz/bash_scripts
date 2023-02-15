f7a6dab60736452beb3a0802146db4708124b933
afnan <dev.afoyez@gmail.com>
2023-02-14 22:47:25 -0500

    initial commit

---
+++ multiplefilesappear.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#NUMFILES=$(ls . | wc -l)
+#echo ${numfiles}
+
+read -p "Set file name: " FILENAME
+read -p "How many files would you like to create? " NUM
+MAXFILENUM=$(ls | egrep -o [0-9]+ | sort -rn | head -n1)
+
+for ((i=1 ; i <= $NUM; i++))
+do
+CURRENTFILENAME=$(($MAXFILENUM+$i))
+touch ${FILENAME}${CURRENTFILENAME}.txt
+done
