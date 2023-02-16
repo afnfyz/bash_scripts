5c07d9c0baa832816876bc03d6757d1c899cdfd6
afnan <dev.afoyez@gmail.com>
2023-02-15 22:36:12 -0500

    minor changes

--- fileproducer.sh
+++ fileproducer.sh
@@ -10,11 +10,13 @@
 
 read -p "Set file name: " FILENAME
 read -p "How many files would you like to create? " NUM
+
 #MAXFILENUM=$(ls | egrep -o [0-9]+ | sort -rn | head -n1)
-MAXFILENUM=$(ls | grep ${FILENAME} | egrep -o [0-9]+ | sort -rn | head -n1)
 #LEASTFILENUM=$(ls | grep ${FILENAME} | egrep -o [0-9]+ | sort -rn | tail -n1)
 
-for ((i=1 ; i <= $NUM; i++))
+MAXFILENUM=$(ls | grep ${FILENAME} | egrep -o [0-9]+ | sort -rn | head -n1)
+
+for ((i=1 ; i <= $NUM ; i++))
 do
 CURRENTFILENUM=$(($MAXFILENUM+$i))
 touch ${FILENAME}${CURRENTFILENUM}.txt
