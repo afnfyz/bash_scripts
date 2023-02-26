074190a87d73d936d310991ea9bb404571b04fe3
afnan <dev.afoyez@gmail.com>
2023-02-25 23:56:51 -0500

    reminder

--- reminder.sh
+++ reminder.sh
@@ -2,25 +2,36 @@
 
 # Display welcome message.
 cat ./ascii/reminder.txt
-echo ""
 
-# Check for at command.
+# Check if which command finds the at command.
 # The /dev/null removes the PATH output from the which command.
-if which at > /dev/null;
-	 then
+if ! which at > /dev/null; # The ! is the NOT logical operator which reverses the exit status of command.
+	then
+  echo "Error: at command not installed."
+  echo "Please install or use crontab to set reminders"
+  exit
+fi
 
 # Set msg variable from input.
 read -p "Enter reminder message: " msg
 echo ""
 
 # Set time variable from input.
-echo "Remind you in how long?"
-echo "(e.g. 1 hour or 30 minutes or 1 hour + 30 minutes etc)"
-read time
-	else
-echo "at command not installed."
-echo "Please install or use crontab to set reminders"
+#echo "Remind you in how long?"
+#echo "(e.g. 1 hour or 30 minutes or 1 hour + 30 minutes etc)"
+#read time
 
-fi
-at now + ${time}  
+# Set time variable from input.
+echo "Enter when you would like to be reminded."
+read datetime
+echo ""
 
+# Convert datetime to at format.
+# The date command is used to convert the date and time into a different format.
+# The -d option tells date to use the value of the $datetime variable as the input date and time
+# The +"%m/%d/%Y %H:%M" option specifies the output format that the command should use.
+#at_time=$(date -d "$datetime" +"%Y%m%d%H%M")
+
+# Set reminder using at command.
+at $datetime touch reminder.txt /home/${USER}/ && cat "Reminder Message: ${msg}" > /home/${USER}/reminder.txt && cat reminder.txt
+echo "Reminder set for $datetime."
