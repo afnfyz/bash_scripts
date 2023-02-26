b08e2808ded285973f419cf1f5a2f28eae96dd2b
afnan <dev.afoyez@gmail.com>
2023-02-25 23:51:23 -0500

    added filter by name

--- multiplefilesappear.sh
+++ multiplefilesappear.sh
@@ -5,7 +5,8 @@
 
 read -p "Set file name: " FILENAME
 read -p "How many files would you like to create? " NUM
-MAXFILENUM=$(ls | egrep -o [0-9]+ | sort -rn | head -n1)
+#MAXFILENUM=$(ls | egrep -o [0-9]+ | sort -rn | head -n1)
+MAXFILENUM=$(ls | grep ${FILENAME} | egrep -o [0-9]+ | sort -rn | head -n1)
 
 for ((i=1 ; i <= $NUM; i++))
 do
