0f5b37f099b5686cfc1c197112ab4cf70e93064a
afnan <dev.afoyez@gmail.com>
2023-03-01 18:35:08 -0500

    eidited functions in lab4.sh

--- bashlabs/afn/lab4.sh
+++ bashlabs/afn/lab4.sh
@@ -7,31 +7,29 @@ read -p "Width: " w
 read -p "Height: " h
 echo ""
 
-volume() {
+function volume() {
     vol=$(echo "$w*$h*$l" | bc)
     echo "Volume = $vol"
 }
-volume
 
-
-surface_area() {
+function surface_area() {
     #echo treats everything as strings, if piped through | bc it can interpret arithmetic expressions. 
     #so you can do echo "3*4" | bc and the output will be 12
     sa=$(echo "2*(($l*$w)+($l*$h)+($w*$h))" | bc)
     echo "Surface Area = $sa"
 }
-surface_area
 
-
-diagonal_base() {
+function diagonal_base() {
     db=$(echo "sqrt(($l^2)+($w^2))" | bc -l )
     echo "Diagonal Base = $db"
 }
-diagonal_base
 
-diagonal_length() {
+function diagonal_length() {
     dl=$(echo "sqrt(($l^2)+($w^2)+($h^2))" | bc -l )
     echo "Diagonal Length = $dl"
 }
-diagonal_length
 
+volume
+surface_area
+diagonal_base
+diagonal_length
