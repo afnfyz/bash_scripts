c703b40f22617d3c7dc23240374abe055a1b7ed4
afnan <dev.afoyez@gmail.com>
2023-02-28 22:40:02 -0500

    lab4 integer and floats

--- bashlabs/afn/lab4.sh
+++ bashlabs/afn/lab4.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
 echo ""
-echo "Find Surface Area and Volume of a Box"
-read -p "Length: " length
-read -p "Width: " width
-read -p "Height: " height
+echo "Find Surface Area, Volume and Diagonals of a Box"
+read -p "Length: " l
+read -p "Width: " w
+read -p "Height: " h
 echo ""
 
 volume() {
-    vol=$(echo "$width*$height*$length" | bc)
+    vol=$(echo "$w*$h*$l" | bc)
     echo "Volume = $vol"
 }
 volume
 
+
 surface_area() {
     #echo treats everything as strings, if piped through | bc it can interpret arithmetic expressions. 
-    surfacearea=$(echo "2*(($length*$width)+($length*$height)+($width*$height))" | bc)
-    echo "Surface Area = $surfacearea"
+    #so you can do echo "3*4" | bc and the output will be 12
+    sa=$(echo "2*(($l*$w)+($l*$h)+($w*$h))" | bc)
+    echo "Surface Area = $sa"
 }
 surface_area
+
+
+diagonal_base() {
+    db=$(echo "sqrt(($l^2)+($w^2))" | bc -l )
+    echo "Diagonal Base = $db"
+}
+diagonal_base
+
+diagonal_length() {
+    dl=$(echo "sqrt(($l^2)+($w^2)+($h^2))" | bc -l )
+    echo "Diagonal Length = $dl"
+}
+diagonal_length
+
