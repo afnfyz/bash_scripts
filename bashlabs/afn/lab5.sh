0d724662cb143ab8372e50074394bb4c2196e5b8
afnan <dev.afoyez@gmail.com>
2023-03-02 23:35:53 -0500

    added to array loop in lab5.sh

--- bashlabs/afn/lab5.sh
+++ bashlabs/afn/lab5.sh
@@ -2,20 +2,23 @@
 
 file="./files/lab5.txt"
 
-# Declare an empty array
+# Have to first declare an empty array
 my_array=()
 
-# Read the file into the array
-i=0
+# Then reads the file into the array
+a=0
 while read -r line; do
-  my_array[i++]=$line
+  my_array[a++]=$line
 done < "$file"
 
-# Filter the array line by line
+# Filters the array line by line
+# The ! allows to iterate over the indices of the array and echo both the index and value.
 for i in "${!my_array[@]}"; do
-  if [[ ! ${my_array[i]} =~ ^[0-9] ]]; then
+  if [[ ${my_array[i]} =~ ^[^A-Za-z0-9_] ]]; then
+    echo "Line starts with a non-word character: ${my_array[i]}"
+  elif [[ ! ${my_array[i]} =~ ^[0-9] ]]; then
     echo "Line $i does not begin with a number: ${my_array[i]}"
   elif [[ ${my_array[i]} =~ ^[0-9] ]]; then
     echo "Line $i begins with a number: ${my_array[i]}"
-  fi
+ fi
 done
