51992fa3cd120ea9335a089cfca8227468884c57
afnan <dev.afoyez@gmail.com>
2023-03-03 22:41:23 -0500

    tarflags.sh

--- tarflags.sh
+++ tarflags.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 compress(){
-tar -czf "${farg}" $1 $2 $3
+tar -czf "${farg}" "$1" "$2" "$3"
 }
 
 while getopts "f:" OPTION; do
     case "$OPTION" in
 	f)
 	  farg="$OPTARG"
-	  compress()
+	  compress
 	  ;;
 	*)
 	  echo "Script Requirements: [-f <file name>]" 
