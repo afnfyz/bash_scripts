cbb28a83d82c5b2bf8c6132daef9cfeb9e26962f
afnan <dev.afoyez@gmail.com>
2023-03-05 18:24:58 -0500

    using csv file instead of tsv

--- mass.sh
+++ mass.sh
@@ -6,25 +6,30 @@ Date: 03/05/23
 Description: 
 '
 
-# Convert csv file to tsv
+#Convert csv file to tsv
 #cat file.csv | sed 's/,/\t/g' > file.tsv
 
-file="./contacts.csv"
+#file="./contacts.csv"
 
 i=0
-while IFS=',' read -r line; do
-  array[i++]=$line
-done < ./contacts.tsv
+while IFS=, read -r name num mail; do
+  name_array[i++]=$name
+  number_array[i++]=$num
+  email_array[i++]=$mail
+done < ~/Downloads/contacts.csv
 
+echo ${name_array[@]}
+
+: '
 for contact in "${array[@]}"; do
   # Extract the name, phone, and email fields from the tab-separated line
   name=$(echo "$contact" | cut -f1)
   phone=$(echo "$contact" | cut -f2)
   email=$(echo "$contact" | cut -f3)
 
- echo "Name: $name"
+  echo "Name: $name"
   echo "Phone: $phone"
   echo "Email: $email"
   echo "---"
 done
-
+'
