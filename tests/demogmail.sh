9007cdd6974c9c9302c2064bb66463d232d9aceb
afnan <dev.afoyez@gmail.com>
2023-03-07 13:40:18 -0500

    bash script using curl with google's gmail api to send bulk emails

--- tests/demogmail.sh
+++ tests/demogmail.sh
@@ -4,10 +4,10 @@
 EMAIL_CONTENT=$(cat ./email_body.txt)
 
 # Set the email address for the recipient
-TO_EMAIL=""
+TO_EMAIL="dev.afoyez@gmail.com"
 
 # Set the email address for the sender
-FROM_EMAIL=""
+FROM_EMAIL="dev.afoyez@gmail.com"
 
 # Set the email subject
 SUBJECT="Automated test"
@@ -27,8 +27,19 @@ EOF
 # Base64 encode the email message as URL-safe
 EMAIL_ENCODED=$(echo -n "${EMAIL}" | base64 -b 0)
 
+# Set the permanent refresh token and client ID/secret
+REFRESH_TOKEN=
+CLIENT_ID=
+CLIENT_SECRET=
+
+# Get a new access token with the refresh token
+ACCESS_TOKEN=$(curl -s \
+  --request POST \
+  --data 
+"client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFR$
+  https://oauth2.googleapis.com/token | jq -r .access_token)
+
 # Send the email using the Gmail API
-ACCESS_TOKEN=""
 curl \
   --request POST \
   --url 'https://gmail.googleapis.com/gmail/v1/users/me/messages/send' \
