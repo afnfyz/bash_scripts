02604c9f81679094c2396d5deedbfdd9678787bf
afnan <dev.afoyez@gmail.com>
2023-04-03 15:54:47 -0400

    rndm.sh

--- practice/rndm.sh
+++ practice/rndm.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo $((1 + $RANDOM % 100))
+num=$((1 + $RANDOM % 15))
+
+i=1
+while [[ $i -le 5 ]]
+do
+read -p "Guess which number I am thinking of between 1 and 15: " input
+
+if [[ $input == $num ]]
+then 
+echo "That is correct! I was thinking of ${num}"
+break
+else 
+echo "That is incorrect."
+
+((i++))
+fi
+
+if [[ $i -eq 6 ]]
+then
+echo "I was actually thinking of ${num}."
+fi
+
+done
+
