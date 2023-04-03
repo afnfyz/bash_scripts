b1be19a11087081dbb40e12ff7fa952490daddf1
afnan <dev.afoyez@gmail.com>
2023-04-03 11:30:53 -0400

    quote.sh

--- functional/quote.sh
+++ functional/quote.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-input=$1
+string=$(curl -Ss https://favqs.com/api/qotd)
+author=$(echo $string | jq -r '.quote.author')
+quote=$(echo $string | jq  '.quote.body')
 
-
+echo ""
+echo $quote
+echo "- ${author}"
+echo ""
